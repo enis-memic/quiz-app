fb7531bbab58fbd09ff72c70c75035800514b0c3
Enis <enis.mema@gmail.com>
2023-09-06 14:06:14 +0200

    Add simple form and devise to the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resources :questions
-  resources :quizzes
+  devise_for :users
+  resources :quizzes do
+    resources :questions
+    resources :quiz_submission, only: [:show, :create], as: 'submissions'
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'quizzes#index'
 end
