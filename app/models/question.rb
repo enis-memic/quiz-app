981eb6f19f6742fb4e06f04601fec19d70cfcf99
Enis <enis.mema@gmail.com>
2023-09-06 14:05:32 +0200

    Modify models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :quiz
+  valdiates :correct_answer, inclusion: { in: 1..4 }
+
+  has_one_attached :image
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,2 +1,8 @@
 class Quiz < ApplicationRecord
+    has_many :questions, dependent: :destroy
+    accepts_nested_attributes_for :questions, reject_if: :all_blank, allow_destroy: true
+
+    def correct_answer_text
+        send("answer#{correct_answer}")
+    end
 end
